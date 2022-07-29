59b212703bd822d03e96b1500d45f8e515379385
Ishanka D Senevirathne <ishankadsenevirathne@gmail.com>
2022-07-29 17:40:34 +0530

    db integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import {prisma} from "../db/client"
 
-const Home: NextPage = () => {
+const Home: NextPage = (props:any) => {
   return (
     <div >
       <Head>
@@ -16,9 +15,22 @@ const Home: NextPage = () => {
         <h1 className="text-5xl">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <code>{props.questions}</code>
       </main>
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps = async () =>{
+
+  const questions = await prisma.pollQuestion.findMany();
+
+  return {
+    props:{
+      questions:JSON.stringify(questions)
+    }
+  }
+
+}
