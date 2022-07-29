4b2120da462f119816478cb28acbc4d132c7751e
Ishanka D Senevirathne <ishankadsenevirathne@gmail.com>
2022-07-29 23:20:53 +0530

    fixing cors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,9 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url ='/api/trpc';
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : 'http://localhost:3000/api/trpc';
 
     return {
       url,
