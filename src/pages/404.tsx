b4b9c0f61e5a6fa0e7af90e69197ff040dfbc6f5
Ishanka D Senevirathne <ishankadsenevirathne@gmail.com>
2022-07-31 19:37:49 +0530

    handling 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,29 @@
+import { NextPage } from "next"
+import Link from "next/link"
+
+const NotFoundPage: NextPage = () => {
+    return (
+      <div className="w-screen h-screen flex flex-col items-center justify-center">
+        <div className="border border-pink-300 p-4 container max-w-screen-md rounded-md text-center">
+            <div className="my-10 flex flex-col items-center justify-center">
+                <p className="text-4xl font-bold">
+                    ERROR : 404 
+                </p>
+                <p className="text-2xl">
+                    Page Not Found
+                </p>
+            </div>
+            <div className="my-10">
+                <Link href="/">
+                    <span className="bg-pink-300 cursor-pointer hover:bg-pink-400 duration-300 delay-10 p-4 text-md font-bold rounded-md">
+                        Go to Dashboard
+                    </span>
+                </Link>
+            </div>
+        </div>
+      </div>
+    )
+  }
+  
+  export default NotFoundPage
+  
