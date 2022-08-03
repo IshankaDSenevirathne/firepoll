9226e2220a078cb43e03fcee79745a65223451bf
Ishanka D Senevirathne <ishankadsenevirathne@gmail.com>
2022-08-03 22:02:51 +0530

    create context success

--- src/backend/router/context.ts
+++ src/backend/router/context.ts
@@ -1,7 +1,7 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 
-export async function createContext(opts?:trpc.CreateNextContextOptions){
+export async function createContext(opts?:trpcNext.CreateNextContextOptions){
     return {token:opts?.req.cookies['poll-token']}
 }
 
